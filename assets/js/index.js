5e5822a34525afd1c195c5e6a104f7d9834460e5
CesarTun <ctuncetina@gmail.com>
2023-07-21 18:26:08 -0600

    feat: agrega funcionalidad completa a la barra de busqueda

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,12 +14,12 @@ let URL = "https://pokeapi.co/api/v2/pokemon/";
 /*------------------------------------------------------------------------------*/
 const chargePokemons = async (maxQuery) => {
     try {
-        for(let i = 1; i <= maxQuery; i++) {
+        for (let i = 1; i <= maxQuery; i++) {
             const pokemon = await fetch(`${URL}${i}`);
-            if(!pokemon.ok) {
+            if (!pokemon.ok) {
                 throw new Error('Error de transferencia de datos');
             }
-            const pokemonJson =  await pokemon.json();
+            const pokemonJson = await pokemon.json();
             mostrarPokemon(pokemonJson);
         }
     } catch (err) {
@@ -68,13 +68,13 @@ const filterPokemon = async (event, maxQuery) => {
     // Borra elementos de la lista
     listaPokemon.innerHTML = '';
     try {
-        for(let i = 1; i <= maxQuery; i++) {
+        for (let i = 1; i <= maxQuery; i++) {
             const pokemon = await fetch(`${URL}${i}`);
-            if(!pokemon.ok) {
+            if (!pokemon.ok) {
                 throw new Error('Error de transferencia de datos');
             }
-            const pokemonJson =  await pokemon.json();
-            
+            const pokemonJson = await pokemon.json();
+
             // Condicional para los tipos
             if (botonId === 'ver-todos') {
                 mostrarPokemon(pokemonJson);
@@ -91,11 +91,98 @@ const filterPokemon = async (event, maxQuery) => {
 }
 
 /*------------------------------------------------------------------------------*/
+/*  Función para abrir opciones de filtrado en botón de búsqueda                */
+/*------------------------------------------------------------------------------*/
+const openFilterOptions = () => {
+    const filterOptions = document.querySelector('#open-filters');
+    const filterBox = document.querySelector('.filters-box');
+
+    filterOptions.addEventListener('click', () => {
+        filterBox.classList.toggle('active-hidden');
+    })
+}
+
+/*------------------------------------------------------------------------------*/
+/*  Función activar filtrado de la barra de búsqueda                            */
+/*------------------------------------------------------------------------------*/
+const activeFilterBar = () => {
+    const input = document.querySelector('#inputSearchPokemon');
+    input.addEventListener('keyup', (event) => {
+        displayPokemonCoincidents(event, listaPokemon, 151);
+    });
+}
+
+/*------------------------------------------------------------------------------*/
+/*  Función para el filtrado de la barra de búsqueda                            */
+/*------------------------------------------------------------------------------*/
+const displayPokemonCoincidents = async (event, container, maxQuery) => {
+    container.innerHTML = '';
+
+    // Variables de filtrado
+    let input = event.target.value.trim();
+    let pokemonList = [];
+
+    if (input === "") {
+        chargePokemons(151);
+        return; // Salir de la función
+    }
+
+    try {
+        for (let i = 1; i <= maxQuery; i++) {
+            const pokemon = await fetch(`${URL}${i}`);
+            if (!pokemon.ok) {
+                throw new Error('Error de transferencia de datos');
+            }
+            const pokemonJson = await pokemon.json();
+            pokemonList.push(pokemonJson);
+        }
+
+        const matchPokemon = filterSBPokemon(pokemonList, input);
+        console.log(matchPokemon);
+        matchPokemon.forEach(pokemon => mostrarPokemon(pokemon));
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+function filterSBPokemon (pokemonArray, input) {
+    // Obtiene los valores de las checkbox
+    const ablePokemonName = document.querySelector('#forPokemon').checked;
+    const ablePokemonNumber = document.querySelector('#forNumber').checked;
+
+    // Normalizar input de entrada: (Árbol || árbol || áRbOl) => arbol
+    const cleanedInput = input.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    // Crea una expresión regular para búsqueda.
+    let regularExpresion = new RegExp(cleanedInput, "i");
+
+    const matchArray = pokemonArray.filter(pokemon => {
+        const normalizedPokemonName = pokemon.name.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+        if (ablePokemonNumber && ablePokemonName) {
+            return regularExpresion.test(normalizedPokemonName) || regularExpresion.test(pokemon.id.toString());
+        } else if (ablePokemonName) {
+            return regularExpresion.test(normalizedPokemonName);
+        } else if (ablePokemonNumber) {
+            return regularExpresion.test(pokemon.id.toString());
+        } else {
+            // Si ambos flags son false, no se aplica ningún filtro, así que devolvemos true para mantener todos los elementos.
+            return true;
+        }
+    });
+
+    return matchArray;
+}
+
+/*------------------------------------------------------------------------------*/
 /*  Inicializadores del DOM                                                     */
 /*------------------------------------------------------------------------------*/
 
 //Inicializa la funcion de carga de Pokemon inicial
 chargePokemons(151);
+// Inicializa la funcion para las opciones de filtrado del botón
+activeFilterBar();
+// Activa la función de filtrato por medio de la barra de búsqueda
+openFilterOptions();
 // Activa la función de filtrado de los botones
 headerButtons.forEach(boton => boton.addEventListener('click', event => filterPokemon(event, 151)));
 
